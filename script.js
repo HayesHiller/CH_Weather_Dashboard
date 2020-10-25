cdd5cfcba4b448712f80252643a264e20531e55b
HayesHiller <hayeshiller@hotmail.com>
2020-10-25 12:54:10 -0600

    adding in my functions

---
+++ script.js
@@ -0,0 +1,13 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidtyEl = document.getElementById("humidty");
+    const currentWindEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    console.log(searchHistory);
+}
